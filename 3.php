7920f737e007da2acd76814233704842886580c0
Nawathpolkaw <nawathpolkaw@gmail.com>
2021-06-08 10:11:20 +0700

    Done 8/6/2021

--- 3.php
+++ 3.php
@@ -1,90 +1,92 @@
 <a name=sec3>
-        <center  style="padding-right: 500px;">
-          <select class="form-select-indicator" style="width: 90px; height: 30px;" aria-label="Default select example">
-              <option selected >ประเทศไทย</option>
-             
-                  
-                  <option value="1" >
-                    : พม่า
-                  </option>
-
-                  <option value="2">
-                    : อังกฤษ
-                  </option>
-
-                  <option value="3">
-                    : เกาหลี
-                  </option>
-
-                  <option value="4">
-                     : จีน
-                  </option>
-
-                  <option value="5">
-                    : ญี่ปุ่น
-                  </option>
-
-                  <option value="6">
-                    : สเปน
-                  </option>
-          </select>&nbsp;&nbsp;&nbsp;
+    <center style="padding-right: 500px;">
+        <select class="form-select-indicator" style="width: 90px; height: 30px;" aria-label="Default select example">
+            <option selected>ประเทศไทย</option>
 
 
-          <select class="form-select-indicator" style="width: 90px; height: 30px;" aria-label="Default select example">
-                <option selected >พารามิเตอร์</option>
+            <option value="1">
+                : พม่า
+            </option>
 
-                  <option value="1" >
-                    : อุณหภูมิ(C)
-                  </option>
+            <option value="2">
+                : อังกฤษ
+            </option>
 
-                  <option value="2" >
-                    : ความชื้นในอากาศ(%)
-                  </option>
+            <option value="3">
+                : เกาหลี
+            </option>
 
-                  <option value="3" >
-                    : ความชื้นในดิน(%)
-                  </option>
+            <option value="4">
+                : จีน
+            </option>
 
-                  <option value="4" >
-                    : เเสง(lux)
-                  </option>
+            <option value="5">
+                : ญี่ปุ่น
+            </option>
 
-                  <option value="5" >
-                    : คุณภาพน้ำ(%)
-                  </option>
-
-                  <option value="6" >
-                    : ระดับน้ำ(%)
-                  </option>
-          </select>&nbsp;&nbsp;&nbsp;
+            <option value="6">
+                : สเปน
+            </option>
+        </select>&nbsp;&nbsp;&nbsp;
 
 
-              <select class="form-select-indicator" style="width: 90px; height: 30px;" aria-label="Default select example">
-                  <option selected ><h1>ทุกช่วงเวลา</h1></option>
-            
-                  <option value="1" >
-                    : 1 วันล่าสุด
-                  </option>
+        <select class="form-select-indicator" style="width: 90px; height: 30px;" aria-label="Default select example">
+            <option selected>พารามิเตอร์</option>
 
-                  <option value="2" >
-                    : 1 สัปดาห์ล่าสุด
-                  </option>
+            <option value="1">
+                : อุณหภูมิ(C)
+            </option>
 
-                  <option value="3" >
-                    : 1 เดือนล่าสุด
-                  </option>
+            <option value="2">
+                : ความชื้นในอากาศ(%)
+            </option>
 
-                  <option value="4" >
-                  : 1 ไตรมาสล่าสุด
-                  </option>
+            <option value="3">
+                : ความชื้นในดิน(%)
+            </option>
 
-                  <option value="5" >
-                    : 1 ปีล่าสุด
-                  </option>
-              </select>
+            <option value="4">
+                : เเสง(lux)
+            </option>
 
-        </center>
+            <option value="5">
+                : คุณภาพน้ำ(%)
+            </option>
 
-        <center>
-            <div id="curve_chart" style="width: 100%; height: 600px;   " ></div> 
-        </center>&nbsp;&nbsp;&nbsp;  
+            <option value="6">
+                : ระดับน้ำ(%)
+            </option>
+        </select>&nbsp;&nbsp;&nbsp;
+
+
+        <select class="form-select-indicator" style="width: 90px; height: 30px;" aria-label="Default select example">
+            <option selected>
+                <h1>ทุกช่วงเวลา</h1>
+            </option>
+
+            <option value="1">
+                : 1 วันล่าสุด
+            </option>
+
+            <option value="2">
+                : 1 สัปดาห์ล่าสุด
+            </option>
+
+            <option value="3">
+                : 1 เดือนล่าสุด
+            </option>
+
+            <option value="4">
+                : 1 ไตรมาสล่าสุด
+            </option>
+
+            <option value="5">
+                : 1 ปีล่าสุด
+            </option>
+        </select>
+
+    </center>
+
+    <center>
+        <div id="curve_chart" style="width: 100%; height: 600px;   "></div>
+    </center>&nbsp;&nbsp;&nbsp;
